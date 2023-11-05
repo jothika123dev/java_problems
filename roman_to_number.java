74391cb67e280f7c87f3b86263f1e47686c95500
jothika123dev <149703232+jothika123dev@users.noreply.github.com>
2023-11-05 20:44:03 +0530

    Update roman_to_number.java

--- roman_to_number.java
+++ roman_to_number.java
@@ -1,6 +1,6 @@
 public class RomanToInteger {
     public static void main(String[] args) {
-        String roman = "IX"; // Replace with your Roman numeral
+        String roman = "IX"; 
 
         int result = romanToInteger(roman);
         System.out.println("Integer value: " + result);
